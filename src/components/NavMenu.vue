7fec25838244db88e61de119b670ad28ea5697bd
renan-andrade-senai <renan.andrade@edu.sc.senai.br>
2023-11-13 17:28:18 -0300

    Update NavMenu.vue

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -6,26 +6,42 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <RouterLink class="nav-link active text-light" to="/avaliacoes">Avaliações</RouterLink>
+                        <RouterLink v-if="modoRestaurante" to="/dadosRestaurante" class="nav-link text-light">Dados Restaurante</RouterLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-light" href="#">Features</a>
+                        <RouterLink v-if="modoRestaurante" to="/cardapio" class="nav-link text-light">Cardápio</RouterLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-light" href="#">Pricing</a>
+                        <RouterLink v-if="modoCliente" class="nav-link active text-light" to="/avaliacoes">Avaliações</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink v-if="modoEntregador" to="/entregas" class="nav-link text-light">Entregas</RouterLink>
                     </li>
                 </ul>
+                <span class="nav-item">
+                    <a class="nav-link text-light">Logout</a>
+                </span>
             </div>
         </div>
     </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { useStore } from "@/stores/appStore";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
-    name: "NavMenu"
+    name: "NavMenu",
+    setup() {
+        const store = useStore();
+        const modoRestaurante = computed(() => store.usuario.tipoUsuario == 'R');
+        const modoCliente = computed(() => store.usuario.tipoUsuario == 'C');
+        const modoEntregador = computed(() => store.usuario.tipoUsuario == 'E');
+        return {
+            modoCliente, modoEntregador, modoRestaurante
+        }
+    }
 })
 </script>
